a3a5930a96d391e76efea719fffe1bb79ceeb84e
Justin/ <justinmandia25@gmail.com>
2026-02-15 20:02:23 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // When  the use click the buttom, toggle between hiding and showing the dropdown
 function myFunction() {
-    document.getElementById('myDropdown').classList.toggle('show');
+    document.getElementById('list-dropdown').classList.toggle('show');
 }
 
 // Close the dropdown menu if the user clicks outside of dropdown
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.drop-btn')) {
         var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
         for ( i = 0; i < dropdowns.length; i++) {
