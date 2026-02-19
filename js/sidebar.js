bce60159cdae21ca2808e7fde5835d12016ba57f
Justin/ <justinmandia25@gmail.com>
2026-02-19 20:05:07 +0800

    Add files via upload

---
+++ js/sidebar.js
@@ -0,0 +1,8 @@
+// Sidebar Menu when the user click the sidebar buton it will display this sidebar
+function myFunction() {
+    document.getElementById('sidebar').style.width = "100%";
+}
+
+function closeFunction() {
+    document.getElementById('sidebar').style.width = "0";
+}
