d7ae318670fe8fc5a34cd52299eaae0bb67d602b
Justin/ <justinmandia25@gmail.com>
2026-02-21 17:30:49 +0800

    Add files via upload

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,3 +6,29 @@ function myFunction() {
 function closeFunction() {
     document.getElementById('sidebar').style.width = "0";
 }
+
+
+// Sidebar Search when the user click the search.
+function openSearch() {
+    document.getElementById('searchBar').style.display = "inline-block";
+}
+
+function closeSearch() {
+    document.getElementById('searchBar').style.display = "none";
+}
+
+// Dropdown Menu on the Sidebar.
+var dropdown = document.getElementById('dropdown-btn');
+var i = 0;
+
+for ( i; i < dropdown.length; i++ ) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle('active');
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "flex") {
+            dropdownContent.style.display === "none";
+        } else {
+            dropdownContent.style.display = "flex";
+        }
+    });
+}
