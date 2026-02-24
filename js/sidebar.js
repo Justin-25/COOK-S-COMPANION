8d145eb87c330105b88ec817a5a8daee12f2769c
unknown <justinmandia25@gmail.com>
2026-02-24 22:26:54 +0800

    Updated cuisine pagination

--- js/sidebar.js
+++ js/sidebar.js
@@ -32,3 +32,70 @@ for ( i; i < dropdown.length; i++ ) {
         }
     });
 }
+
+// Pagination of the Menu Cuisine
+let currentPage = 1;
+const totalPages = 5;
+
+const paginationContainer = document.querySelector(".pagination nav ul");
+let paginationPage = document.querySelector(".pagination-page");
+const prevBtn = document.querySelector(".prev-pagination");
+const nextBtn = document.querySelector(".next-pagination");
+
+function renderPagination() {
+
+    // Remove old page buttons
+    let pageBtn = document.querySelectorAll('.pagination-page');
+    pageBtn.forEach(function(button) {
+        button.parentElement.remove();
+    });
+
+    // Add new page button
+    let nextLi = nextBtn.parentElement; 
+
+    // Create list item
+    let createLi = document.createElement('li');
+
+    // Create button
+    let createBtn = document.createElement('button');
+    createBtn.className = 'pagination-page active';
+    createBtn.textContent = currentPage;
+
+    // Assemble
+    createLi.appendChild(createBtn);
+
+    // Insert
+    paginationContainer.insertBefore(createLi, nextLi)
+    
+    // Disable prevbtn if on first page
+    if (currentPage === 1) {
+        prevBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+    };
+
+    // Disable nextBtn if on last page
+    if (currentPage === totalPages) {
+        nextBtn.disabled = true;
+    } else {
+        nextBtn.disabled = false;
+    };
+}
+
+prevBtn.addEventListener('click', function() {
+    // Dont go below page 1
+    if (currentPage > 1) {
+        currentPage--;
+        renderPagination();
+    };
+});
+
+nextBtn.addEventListener('click', function() {
+    //Dont go above the totalPages
+    if (currentPage < totalPages) {
+        currentPage++;
+        renderPagination();
+    };
+});
+
+renderPagination();
